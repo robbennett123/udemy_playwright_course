5f4cfdb4e4d58c345433d176e29c8c19b90f294d
Rob Bennett <rob.contract@gmail.com>
2025-12-29 14:24:17 +0000

    Added tests for About page and identifying 'get started' button

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -8,5 +8,42 @@ test.describe('Home', () => {
     //verify title
     await expect(page).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
 })
-    
+
+  test('Open About page and verify title', async ({ page }) => {
+     //open url
+     await page.goto('https://practice.sdetunicorns.com/about');
+
+     //verify title
+     await expect(page).toHaveTitle('About – Practice E-Commerce Site');
 })
+
+test('Click Get Started button using object id', async ({ page }) => {
+     //open url
+     await page.goto('https://practice.sdetunicorns.com');
+
+     //click button
+     await page.click("[id='get-started']");
+
+     //verify URL has #get-started
+        await expect(page).toHaveURL('https://practice.sdetunicorns.com/#get-started');
+
+
+})
+test('Click Get Started button using CSS selector', async ({ page }) => {
+     //open url
+     await page.goto('https://practice.sdetunicorns.com');
+
+     //click button
+     await page.locator('#get-started').click();
+
+     //verify URL has #get-started
+        await expect(page).toHaveURL('https://practice.sdetunicorns.com/#get-started');
+
+
+})
+
+
+})
+    
+
+ 
