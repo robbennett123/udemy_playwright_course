64d41b98ab6650b6ec14c36fbcfcb00b030e0ccd
Rob Bennett <rob.contract@gmail.com>
2026-01-07 11:43:50 +0000

    Added test that has to make a hidden element

--- tests/Cart.spec.ts
+++ tests/Cart.spec.ts
@@ -9,7 +9,7 @@ test('file upload', async ({ page }) => {
     await page.goto('https://practice.sdetunicorns.com/cart/');
 
 // store test file path
-const filePath = path.resolve(__dirname, '../data/Rob Test Upload File.txt');
+const filePath = path.resolve(__dirname, '../testData/pngFile.png');
 
 
 // Upload test file - QUESTION how is this locator being identified? Answer: 1. the # denotes id 
@@ -23,8 +23,38 @@ await page.setInputFiles('input#upfile_1', filePath);
 await page.getByRole('button', { name: 'Upload File' }).click();
 
 // Verify file upload success
-    
+    await expect(page.locator('#wfu_messageblock_header_1_label_1')).toContainText('pngFile.png uploaded successfully');
 })
 
+test('file upload from hidden field', async ({ page }) => {
+
+// Navigate to the cart page
+    await page.goto('https://practice.sdetunicorns.com/cart/');
+
+// store test file path
+const filePath = path.resolve(__dirname, '../testData/pngFile.png');
+
+//DOM manipulation to make the hidden field visible. This finds the hidden class for the hidden elements and removes it - making the element visible
+await page.evaluate(() => {
+    const visibleSelector = document.querySelector('#upfile_1');
+    if (visibleSelector) {
+        visibleSelector.className = '';
+    }
+});
+
+
+// Upload test file - QUESTION how is this locator being identified? Answer: 1. the # denotes id 
+//2. input is the element type 3. upfile_1 is the id value
+// This command would still run as 'await page.setInputFiles('#upfile_1', filePath);'
+//id as an identifier is not the best practice, but we use it here because the field is hidden
+await page.setInputFiles('input#upfile_1', filePath);
+
+
+// click Submit button
+await page.getByRole('button', { name: 'Upload File' }).click();
+
+// Verify file upload success
+    await expect(page.locator('#wfu_messageblock_header_1_label_1')).toContainText('pngFile.png uploaded successfully');
+})
 
 })
