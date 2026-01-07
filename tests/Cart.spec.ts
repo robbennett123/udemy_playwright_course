69633693cdf5f3de254a5fd93f115f1a0c2948f8
Rob Bennett <rob.contract@gmail.com>
2026-01-07 10:18:49 +0000

    Add the data file 'Rob Test Upload File.txt' and some comments

--- tests/Cart.spec.ts
+++ tests/Cart.spec.ts
@@ -12,8 +12,12 @@ test('file upload', async ({ page }) => {
 const filePath = path.resolve(__dirname, '../data/Rob Test Upload File.txt');
 
 
-// Upload test file - QUESTION how is this locator being identified?
+// Upload test file - QUESTION how is this locator being identified? Answer: 1. the # denotes id 
+//2. input is the element type 3. upfile_1 is the id value
+// This command would still run as 'await page.setInputFiles('#upfile_1', filePath);'
+//id as an identifier is not the best practice, but we use it here because the field is hidden
 await page.setInputFiles('input#upfile_1', filePath);
+
 
 // click Submit button
 await page.getByRole('button', { name: 'Upload File' }).click();
