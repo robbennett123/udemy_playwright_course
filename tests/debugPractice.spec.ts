eba17922c02c7f032fa744a7ebdb8a82887d9ab6
Rob Bennett <rob.contract@gmail.com>
2026-01-10 11:21:48 +0000

    updated Debug test with more comments

--- tests/debugPractice.spec.ts
+++ tests/debugPractice.spec.ts
@@ -7,9 +7,11 @@ test.describe('Debug Practice', () => {
 
     //verify title
     await expect(page).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
+
+   await page.pause();
     
-    //Write some failing tests to see how debugging works in the Playwright Trace Viewer and using the terminal logs
-    await expect(page.locator('#get-started')).toContainText('get started');
+    //Write some failing tests to see how debugging works in the Playwright Trace Viewer and using the terminal logs - test below has 'tarted' misspelled
+    await expect(page.locator('#get-started')).toContainText('get tarted');
     //This test will fail because the text is incorrect and the following errors will be logged in the terminal
     //locator resolved to <a id="get-started" href="#get-started" class="elementor-button elementor-button-link elementor-size-sm elementor-animation-grow">…</a> +9ms
     //pw:api   unexpected value "
@@ -17,5 +19,9 @@ test.describe('Debug Practice', () => {
     //This test can be run from the terminal using npx playwright test "debugPractice.spec.ts" -g "Debug\s+Practice\s+\S+"
     //It can also be run in debug mode using the command $env:debug="pw:api"; npx playwright test "debugPractice.spec.ts" -g "Debug\s+Practice\s+\S+"
     //which will log out all Playwright API calls to the terminal
+
+    //Can also trigger the playwright Inspector - either from the terminal using the command $env:pwdebug=1; npx playwright test "debugPractice.spec.ts" -g "Debug\s+Practice\s+\S+"
+    //Or by adding the following line of code to the test to  await page.pause(); This will pause the test at that point in the test and open the Inspector
+
    });
 });
