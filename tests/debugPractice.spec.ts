b95267fe324054e2b34c2d3ba393b1946b5c28b2
Rob Bennett <rob.contract@gmail.com>
2026-01-10 10:29:18 +0000

    Added a deliberately failing assertion to the debug test to see the debug output

--- tests/debugPractice.spec.ts
+++ tests/debugPractice.spec.ts
@@ -7,15 +7,15 @@ test.describe('Debug Practice', () => {
 
     //verify title
     await expect(page).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
-
-})
-
-
-});
-
-//This test can be run from the terminal using npx playwright test "debugPractice.spec.ts" -g "Debug\s+Practice\s+\S+"
-//It can also be run in debug mode using the command $env:debug="pw:api"; npx playwright test "debugPractice.spec.ts" -g "Debug\s+Practice\s+\S+"
-//which will log out all Playwright API calls to the terminal
     
-
- 
+    //Write some failing tests to see how debugging works in the Playwright Trace Viewer and using the terminal logs
+    await expect(page.locator('#get-started')).toContainText('get started');
+    //This test will fail because the text is incorrect and the following errors will be logged in the terminal
+    //locator resolved to <a id="get-started" href="#get-started" class="elementor-button elementor-button-link elementor-size-sm elementor-animation-grow">…</a> +9ms
+    //pw:api   unexpected value "
+    
+    //This test can be run from the terminal using npx playwright test "debugPractice.spec.ts" -g "Debug\s+Practice\s+\S+"
+    //It can also be run in debug mode using the command $env:debug="pw:api"; npx playwright test "debugPractice.spec.ts" -g "Debug\s+Practice\s+\S+"
+    //which will log out all Playwright API calls to the terminal
+   });
+});
